89ce8e4d1d2b79cebeaece2b98d52626ece69d4b
Khadija-Asim049 <khadijaasim283@gmail.com>
2026-07-03 21:41:40 +0500

    Revert "Fix test script to validate incorrect addition outputs"
    This reverts commit bc2ed9371e1f528d6cc33c4bfc2c3ec14c4b60a6.

--- src/calculator/test.sh
+++ src/calculator/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OUTPUT=$(./build/app)
-echo "$OUTPUT" | grep -q "add(3,3) = 5" && \
+echo "$OUTPUT" | grep -q "add(3,2) = 5" && \
 echo "$OUTPUT" | grep -q "subtract(5,1) = 4" && \
 echo "All tests passed." && exit 0
 
